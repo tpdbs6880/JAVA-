80d6bd7d278107f78cff5497a5fb04718a7be94a
Jeon Se Yun <51560615+tpdbs6880@users.noreply.github.com>
2022-07-13 15:41:39 +0900

    commit

--- src/ch11/Hello.java
+++ src/ch11/Hello.java
@@ -1,17 +1,12 @@
 package ch11;
 
-public class HelloJava {
+public class Hello {
 
 	public static void main(String[] args) {
 
+		
 		System.out.println("깃허브 업로드 테스트입니다.");
 		
-	
-	
 	}
 
 }
-
-
-
-
